391fc5789ac35ba5e6ff1f988deb34058fded5a2
jirman rodriguez <87489149+ironiante@users.noreply.github.com>
2022-06-04 19:58:18 -0500

    funcionesParametros

--- src/funciones/funcionesParametros.kt
+++ src/funciones/funcionesParametros.kt
@@ -1,15 +1,25 @@
 package funciones
 
 fun main() {
-    sayMyName("jirman")
+   // sayMyName("jirman")
+   // sayNameAndAge("jirman",41)
+   println(sumtwoNumbers(10,5))
 
 }
+/*
 // primera funcion pidiendo nombre
 private fun sayMyName(name:String){
     println("hola mi nombre es $name")
 
 }
 // funcion con mas de un parametro
-private fun sayNameAndAge(){
+private fun sayNameAndAge(name:String,age:Int){
+    println ("mi nombre es $name y mi edad es $age")
+
+}*/
+// funciones con valor de retorno
+private  fun sumtwoNumbers(firsNumber:Int,secondNumber:Int):Int{
+    var sum = firsNumber+secondNumber
+    return sum
 
 }
